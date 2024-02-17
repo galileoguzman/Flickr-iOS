dd87805f796815874cbd491fb19becad536b1678
galileoguzman <galileoguzman@gmail.com>
2024-02-17 11:11:16 -0600

    Initial Commit

---
+++ Flickr-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flickr-iOS
+//
+//  Created by Galileo Guzman on 17/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flickr-iOS/Flickr_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flickr_iOSApp.swift
+//  Flickr-iOS
+//
+//  Created by Galileo Guzman on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Flickr_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
