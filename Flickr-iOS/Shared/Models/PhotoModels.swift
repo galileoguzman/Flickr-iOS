cc933df32604d09c2e2a22c23472ad85577a43cb
galileoguzman <galileoguzman@gmail.com>
2024-02-17 12:15:43 -0600

    Photo Model | implement computed property
    - new computed property to get another size of the thumbnail

--- Flickr-iOS/Shared/Models/PhotoModels.swift
+++ Flickr-iOS/Shared/Models/PhotoModels.swift
@@ -13,8 +13,16 @@ struct PhotoSearchResponse: Decodable {
     let items: [Photo]
 }
 
+/// There are different versions of the thumbnail for each image
+/// https://www.flickr.com/services/api/misc.urls.html
+/// this struct implements a computed property to transform the m url to a q url replacing only the desired size
 struct Media: Decodable {
     let m: URL
+
+    var q: URL {
+        let url = m.absoluteString.replacingOccurrences(of: "m.jpg", with: "q.jpg")
+        return URL(string: url)!
+    }
 }
 
 struct Photo: Decodable {
